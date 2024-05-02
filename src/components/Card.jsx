c386e82ca9d6eeb1661e77809e28ce0973b652c2
Nakul Chouksey <71.nakul@gmail.com>
2024-05-03 00:44:15 +0530

    thora or dev

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import { FaRegFileAlt } from "react-icons/fa";
+import { LuDownload } from 'react-icons/lu';
 
 function Card() {
   return (
     <div className='w-60 p-5 h-72 rounded-[35px] overflow-hidden bg-zinc-900/90 relative text-white'>
         <FaRegFileAlt />
         <p className='text-sm mt-5 leading-tight font-medium'>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-        <div className='footer absolute bottom-0 bg-sky-600 w-full h-14 left-0'></div>
+        <div className='footer absolute bottom-0 w-full left-0'>
+          <div className='flex items-center py-3 px-6 justify-between mb-2'>
+            <h5>.4mb</h5>
+            <span className='w-7 h-7 bg-zinc-600 rounded-full flex items-center justify-center'>
+              <LuDownload size=".7em" color='#fff'/>
+            </span>
+          </div>
+          <div className='tag w-full flex items-center justify-center py-3 bg-green-600'>
+            <h3 className='text-sm font-semibold'>Download Now</h3>
+          </div>
+        </div>
     </div>
   )
 }
--- src/components/Foregrounud.jsx
+++ src/components/Foregrounud.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import Card from './Card'
 
 function Foregrounud() {
+  // const data = [
+  //   icon, name,
+  // ]
+  useState()
   return (
     <>
         <div className='fixed top-0 left-0 z-[3] w-full h-full'>
