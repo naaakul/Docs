079cfff5ff24ed3e150fd02f574b61d235fe257d
Nakul Chouksey <71.nakul@gmail.com>
2024-05-03 13:57:34 +0530

    fav icon done ✅

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -6,7 +6,7 @@ function background() {
           <div className="absolute top-[5%] w-full py-10 font-semibold flex justify-center text-zinc-600">
             Documents
           </div>
-          <h1 className="text-zinc-900 absolute top-1/2 left-1/2 -translate-x-[50%] font-black -translate-y-[50%] text-[14vw] leading-none tracking-tight">
+          <h1 className="text-zinc-900 absolute top-1/2 left-1/2 -translate-x-[50%] font-normal -translate-y-[50%] text-[14vw] leading-none tracking-tight">
             Docs.
           </h1>
         </>
